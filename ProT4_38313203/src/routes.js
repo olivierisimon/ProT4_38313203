07328c2036948fa23f4d4583d7410f1923cd1f2b
olivierisimon <simon.olivieri01@gmail.com>
2024-08-18 20:33:20 -0300

    Update routes.js
    Se agregan rutas faltantes

--- ProT4_38313203/src/routes.js
+++ ProT4_38313203/src/routes.js
@@ -5,4 +5,7 @@ export const router = Router()
 
 router.get('/libros', libro.getAll);
 router.post('/libro', libro.getOne);
+router.post('/add-libro', libro.add);
+router.delete('/delete-libro', libro.delete);
+router.put('/update-libro', libro.update);
 
