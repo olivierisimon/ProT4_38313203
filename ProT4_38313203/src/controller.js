6b46c4cbf481c00f2658e51977dd847436a978d4
olivierisimon <simon.olivieri01@gmail.com>
2024-08-18 20:32:37 -0300

    Update controller.js
    Se agregan api update, delete y add

--- ProT4_38313203/src/controller.js
+++ ProT4_38313203/src/controller.js
@@ -20,6 +20,43 @@ class LibrosController{
             res.status(500).json({ error: error.message });
         }
     }
+
+    async add(req, res) {
+        try {
+            const libro = req.body;
+            const [result] = await pool.query(
+                `INSERT INTO libros(nombre, autor, categoria, \`año-publicacion\`, ISBN) VALUES (?, ?, ?, ?, ?)`,
+                [libro.nombre, libro.autor, libro.categoria, libro['año-publicacion'], libro.ISBN]
+            );
+            res.json({ "Id insertado": result.insertId });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+    
+    async delete(req, res) {
+        try {
+            const { ISBN } = req.body;
+            const [result] = await pool.query('DELETE FROM libros WHERE ISBN = ?', [ISBN]);
+            res.json({ "Registros eliminados": result.affectedRows });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+    
+    async update(req, res) {
+        try {
+            const libro = req.body;
+            const [result] = await pool.query(
+                `UPDATE libros SET nombre = ?, autor = ?, categoria = ?, \`año-publicacion\` = ?, ISBN = ? WHERE id = ?`,
+                [libro.nombre, libro.autor, libro.categoria, libro['año-publicacion'], libro.ISBN, libro.id]
+            );
+            res.json({ "Registros actualizados": result.changedRows });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+    
     
 
 }
